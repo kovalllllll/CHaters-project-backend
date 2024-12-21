270c40b617eacbdbafdd8490a656807cf375485f
Danylo Bilyi <danil22344bel@gmail.com>
2024-12-21 21:44:31 +0200

    add user service

---
+++ BLL/Services/Impl/UserService.cs
@@ -0,0 +1,29 @@
+using BLL.Exceptions;
+using DAL.Entities;
+using DAL.Repositories;
+
+namespace BLL.Services.Impl;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+    
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
+    public User CreateUser(User user)
+    {
+        if (_userRepository.IsUserWithEmailExists(user.Email))
+        {
+            throw new UserAlreadyExistException($"User with email '{user.Email}' already exists");
+        }
+        
+        var createdUser = _userRepository.Create(user);
+        
+        _userRepository.SaveChanges();
+        
+        return createdUser;
+    }
+}
